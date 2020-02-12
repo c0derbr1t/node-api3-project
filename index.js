ab79706986f617efa82f3a817a099cffef846901
c0derbr1t <c0der.br1t@gmail.com>
2020-02-12 16:11:05 -0600

    Initial Commit. Added dependencies. Built basic server with initial route, and connected to userRouter and postRouter. Built and globally implemented custom logger middleware.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require('./server.js');
+
+server.listen(5500, () => {
+    console.log(`\n* Today's server is running on port 5500! *\n`);
+});
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
+
 const server = express();
+
+server.use(express.json());
+
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
+
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +18,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} request to ${req.originalUrl} at [${new Date().toISOString()}] `);
+  next();
+}
 
 module.exports = server;
